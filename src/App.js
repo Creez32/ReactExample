0c5bc2ca8051dd06a2e7bf0d62c661c01e943219
Creez32 <cristiannicolas32@hotmail.com>
2021-11-05 18:30:46 -0300

    subo a git un proyecto de react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {TodoCounter} from './TodoCounter'
+import {TodoSearch} from './TodoSearch'
+import {TodoItem} from './TodoItem'
+import {CreateTodoButton} from './CreateTodoButton'
+import {TodoList} from './TodoList'
 
-function App() {
+const defaultTodos = [
+  {
+    text : 'Cortar Cebolla',
+    completed : true
+  },
+  {
+    text : 'Hace magia con react',
+    completed : false
+  },
+  {
+    text : 'Llorar por cortar Cebolla',
+    completed : false
+  },
+]
+
+function App(props) {
+
+  const [search,setSearch] = React.useState('')
+  const [todos,setTodos] = React.useState(defaultTodos)
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length
+  
+  let searchedTodos = []
+
+  if(!search.length >= 1){
+    searchedTodos = todos
+  }else{
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase()
+      const searchText = search.toLowerCase()
+      return todoText.includes(searchText)
+    })
+  }
+
+  const completeTodos = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text)
+    const newTodos = [...todos]
+    newTodos[todoIndex].completed = true
+    setTodos(newTodos)
+    /* todos[todoIndex] = {
+      text : todos.todoIndex.text,
+      completed: true,
+    } */
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter 
+      total = {totalTodos}
+      completed = {completedTodos}
+      />
+    <TodoSearch 
+    search = {search}
+    setSearch = {setSearch}
+    />
+    
+    <TodoList>
+      {searchedTodos.map(todo => (
+        <TodoItem 
+        key={todo.text} 
+        text={todo.text}
+        completed={todo.completed}
+        onComplete={() => completeTodos(todo.text)}
+        />
+        ))}
+    </TodoList>
+
+    <CreateTodoButton />
+    
+    </React.Fragment>
   );
 }
 
